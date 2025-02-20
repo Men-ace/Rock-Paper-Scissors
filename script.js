e42ff76fae8fc21cea9e58aac8b5551cf39e5457
Men-ace <chhettrisailesh1@gmail.com>
2025-02-20 15:25:00 +1100

    added choice for scissors and rock

---
+++ script.js
@@ -0,0 +1,8 @@
+function playGame(userChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let result = '';
+    if (userChoice === computerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||}
